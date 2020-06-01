9ab77d54b0f9f616d0725d711898c5d792c17217
itmrchow <itmrchow@gmail.com>
2020-06-01 15:03:34 +0800

    TrainingApp init

---
+++ TrainingApp/src/main/java/com/Training/TrainingApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.Training.TrainingApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TrainingAppApplication.class);
+	}
+
+}
