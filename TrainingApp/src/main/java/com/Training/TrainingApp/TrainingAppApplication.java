97188e152b1cae26a91c14c918e8398603c8cb29
itmrchow <itmrchow@gmail.com>
2020-06-02 16:50:07 +0800

    Spring 設定調整

--- TrainingApp/src/main/java/com/Training/TrainingApp/TrainingAppApplication.java
+++ TrainingApp/src/main/java/com/Training/TrainingApp/TrainingAppApplication.java
@@ -2,8 +2,15 @@ package com.Training.TrainingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.Training")
+@EnableJpaRepositories("com.Training.Dao")
+@EntityScan("com.Training.Model")
+
 public class TrainingAppApplication {
 
 	public static void main(String[] args) {
