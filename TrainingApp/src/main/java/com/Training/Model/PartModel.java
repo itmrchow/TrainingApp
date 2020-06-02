59c63e0e86aef73c7b4e1669d48428f676380342
itmrchow <itmrchow@gmail.com>
2020-06-02 10:34:48 +0800

    create partmodel

---
+++ TrainingApp/src/main/java/com/Training/Model/PartModel.java
@@ -0,0 +1,61 @@
+package com.Training.Model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "part")
+public class PartModel {
+	@Id
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPartsName() {
+		return partsName;
+	}
+
+	public void setPartsName(String partsName) {
+		this.partsName = partsName;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Column(name = "parts_name")
+	private String partsName;
+
+	@Column(name = "create_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+
+	@Column(name = "update_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updateDate;
+
+}
