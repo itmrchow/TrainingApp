efabd213c96dced51f02e59d9c15836efe1e4c06
itmrchow <itmrchow@gmail.com>
2020-06-03 14:48:35 +0800

    Create PartService

---
+++ TrainingApp/src/main/java/com/Training/Service/PartService.java
@@ -0,0 +1,37 @@
+package com.Training.Service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.Training.Model.PartModel;
+
+@Service
+public class PartService {
+
+	// c
+	public PartModel createPart(PartModel part) {
+		return null;
+	}
+
+	// r
+	public PartModel getPart(String id) {
+		return null;
+	}
+
+	// u
+	public PartModel updatePart(PartModel part) {
+		return null;
+	}
+
+	// d
+	public void deletePart(String id) {
+
+	}
+
+	// find
+	public List<PartModel> findParts() {
+		return null;
+	}
+
+}
