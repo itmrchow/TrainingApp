5878759f6a589498c57ce20699276876ac1c54db
itmrchow <itmrchow@gmail.com>
2020-06-05 16:30:47 +0800

    新增findByPartNameLike方法

--- TrainingApp/src/main/java/com/Training/Dao/PartRepository.java
+++ TrainingApp/src/main/java/com/Training/Dao/PartRepository.java
@@ -1,9 +1,11 @@
 package com.Training.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Training.Model.PartModel;
 
 public interface PartRepository extends JpaRepository<PartModel, String> {
-
+	List<PartModel> findByPartNameLike(String partName);
 }
