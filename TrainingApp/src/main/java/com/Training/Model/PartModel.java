51eb0f3f621661d780d506e9aedb1c85a50be836
itmrchow <itmrchow@gmail.com>
2020-06-05 16:28:53 +0800

    create date & update date 設定 @EnableJpaAuditing、@EntityListeners(AuditingEntityListener.class) @CreatedDate @LastModifiedDate 標籤

--- TrainingApp/src/main/java/com/Training/Model/PartModel.java
+++ TrainingApp/src/main/java/com/Training/Model/PartModel.java
@@ -4,39 +4,45 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
 @Table(name = "part")
+@EntityListeners(AuditingEntityListener.class)
 public class PartModel {
 	@Id
 	private String id;
 
-	@Column(name = "parts_name")
-	private String partsName;
+	@Column(name = "part_name")
+	private String partName;
 
-	@Column(name = "create_date")
+	@CreatedDate
 	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "create_date" , updatable = false)
 	private Date createDate;
 
+	@LastModifiedDate
 	@Column(name = "update_date")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date updateDate;
 
-	@PrePersist // save前調用
-	void saveDate() {
-		this.createDate = this.updateDate = new Date();
-	}
-
-	@PreUpdate // update前調用
-	void initUpdateDate() {
-		this.updateDate = new Date();
-	}
+//	@PrePersist // save前調用
+//	void saveDate() {
+//		this.createDate = this.updateDate = new Date();
+//	}
+//
+//	@PreUpdate // update前調用
+//	void initUpdateDate() {
+//		this.updateDate = new Date();
+//	}
 
 	public String getId() {
 		return id;
@@ -46,12 +52,12 @@ public class PartModel {
 		this.id = id;
 	}
 
-	public String getPartsName() {
-		return partsName;
+	public String getPartName() {
+		return partName;
 	}
 
-	public void setPartsName(String partsName) {
-		this.partsName = partsName;
+	public void setPartName(String partName) {
+		this.partName = partName;
 	}
 
 	public Date getCreateDate() {
